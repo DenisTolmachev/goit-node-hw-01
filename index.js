231855dbccf24a8729de369ec75204fe5dde92b0
Denis <denis.tolmachev@gmail.com>
2022-09-24 22:02:30 +0300

    added list, get, add function

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-console.log('Hello, world!');
+//const { Command } = require("commander");
+
+const contacts = require("./contacts");
+
+// const program = new Command();
+// program
+//   .option("-a, --action <type>", "choose action")
+//   .option("-i, --id <type>", "user id")
+//   .option("-n, --name <type>", "user name")
+//   .option("-e, --email <type>", "user email")
+//   .option("-p, --phone <type>", "user phone");
+
+// program.parse(process.argv);
+
+// const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      console.table(oneContact);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      console.table(newContact);
+      break;
+
+    case "remove":
+      // ... id
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+// invokeAction(argv);
+invokeAction({
+  action: "add",
+  name: "Holly Molly",
+  email: "exanple@example.net",
+  phone: "(111) 111-1111",
+});
